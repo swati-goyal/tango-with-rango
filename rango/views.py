cf77d77d97699e6b92b45b975f17ca2db7218231
Swati Goyal <swati_goyal@outlook.com>
2017-10-10 13:02:09 +0530

    Basic URL mappings
    Implemented URL mappings in the rango project and it shows links to about page.

---
+++ rango/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Rango says hello world! <a href=about/>About</a>")
+
+
+def about(request):
+    return HttpResponse("Rango Says: Here is the about page. <a href=/rango/>Index</a>")
+
+
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from . import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^rango/', include('rango.urls')),
+    url(r'^$', views.main_index, name='main_index'),
 ]
