2fed032b1caa8837febefbe8e6282befdea07bf9
Swati Goyal <swati_goyal@outlook.com>
2017-10-16 17:39:35 +0530

    Forms and django templates.
    Implementation of forms.

--- populate_rango.py
+++ populate_rango.py
@@ -5,30 +5,38 @@ def populate():
     python_cat = add_cat('Python')
     add_page(cat=python_cat,
              title="Official Python Tutorial",
-             url="http://docs.python.org/2/tutorial/")
+             url="http://docs.python.org/2/tutorial/",
+             views=23)
     add_page(cat=python_cat,
              title="How to Think like a Computer Scientist",
-             url="http://www.greenteapress.com/thinkpython/")
+             url="http://www.greenteapress.com/thinkpython/",
+             views=63)
     add_page(cat=python_cat,
              title="Learn Python in 10 Minutes",
-             url="http://www.korokithakis.net/tutorials/python/")
+             url="http://www.korokithakis.net/tutorials/python/",
+             views=35)
     django_cat = add_cat("Django")
     add_page(cat=django_cat,
              title="Official Django Tutorial",
-             url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+             url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+             views=58)
     add_page(cat=django_cat,
              title="Django Rocks",
-             url="http://www.djangorocks.com/")
+             url="http://www.djangorocks.com/",
+             views=145)
     add_page(cat=django_cat,
              title="How to Tango with Django",
-             url="http://www.tangowithdjango.com/")
+             url="http://www.tangowithdjango.com/",
+             views=196)
     frame_cat = add_cat("Other Frameworks")
     add_page(cat=frame_cat,
              title="Bottle",
-             url="http://bottlepy.org/docs/dev/")
+             url="http://bottlepy.org/docs/dev/",
+             views=54)
     add_page(cat=frame_cat,
              title="Flask",
-             url="http://flask.pocoo.org")
+             url="http://flask.pocoo.org",
+             views=89)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "I am bold font from the context"}
-    return render_to_response('C:\\Users\swati.goyal\\tango_with_django_project\\templates\\rango\\index.html', context_dict, context)
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, "authorname": "Swati Goyal", "pages": page_list}
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
+    return render_to_response('rango/index.html', context_dict, context)
 
 
 def about(request):
@@ -16,3 +22,32 @@ def about(request):
     return render_to_response('C:\\Users\swati.goyal\\tango_with_django_project\\templates\\rango\\about.html', context_dict, context)
 
 
+def category(request, category_name_url):
+    context = RequestContext(request)
+    category_name = category_name_url.replace('_', ' ')
+    context_dict = {'category_name': category_name}
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render_to_response('rango/category.html', context_dict, context)
+
+
+def add_category(request):
+    context = RequestContext(request)
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        # Have we been provided with a valid form?
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+
+    return render_to_response('rango/add_category.html', {'form': form}, context)
