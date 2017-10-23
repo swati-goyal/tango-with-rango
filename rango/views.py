341a950e7d8b4dd12755f0d34304f0102dbd2f43
Swati Goyal <swati_goyal@outlook.com>
2017-10-23 17:42:56 +0530

    Add Page implemented
    User can add a page from category page  using 'Add Page' link. User needs to enter the url and a title of the page.

--- rango/views.py
+++ rango/views.py
@@ -6,25 +6,24 @@ from django.views.decorators.csrf import ensure_csrf_cookie, requires_csrf_token
 
 
 def index(request):
-    context = RequestContext(request)
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, "authorname": "Swati Goyal", "pages": page_list}
     for category in category_list:
         category.url = category.name.replace(' ', '_')
-    return render_to_response('rango/index.html', context_dict, context)
+    return render_to_response('rango/index.html', context_dict, RequestContext(request, {}))
 
 
 def about(request):
     context = RequestContext(request)
     context_dict = {'boldaboutmessage': "This is about page!"}
-    return render_to_response('C:\\Users\swati.goyal\\tango_with_django_project\\templates\\rango\\about.html', context_dict, context)
+    return render_to_response('rango/about.html', context_dict, context)
 
 
 def category(request, category_name_url):
     context = RequestContext(request)
     category_name = category_name_url.replace('_', ' ')
-    context_dict = {'category_name': category_name}
+    context_dict = {'category_name': category_name, 'category_name_url': category_name_url}
     try:
         category = Category.objects.get(name=category_name)
         pages = Page.objects.filter(category=category)
@@ -51,4 +50,25 @@ def add_category(request):
     else:
         form = CategoryForm()
 
-    return render_to_response('rango/add_category.html', {'form': form,}, RequestContext(request, {}))
+    return render_to_response('rango/add_category.html', {'form': form,}, RequestContext(request))
+
+
+def add_page(request, category_name_url):
+    context = RequestContext(request)
+    category_name = category_name_url.replace('_', ' ')
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            page = form.save(commit=False)
+            cat = Category.objects.get(name=category_name)
+            page.category = cat
+            page.views = 0
+            page.save()
+            return category(request, category_name_url)
+        else:
+            print(form.errors)
+    else:
+        form = PageForm()
+
+    return render_to_response('rango/add_page.html', {'category_name_url': category_name_url,
+                                                      'category_name': category_name, 'form': form}, context)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
 ]
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
